d9b59b2c5878dcbba9b5bf933ee01bee23855283
Mintimate <mintimate215@gmail.com>
2023-04-21 22:24:35 +0800

    添加输入限制
    - 参考：https://github.com/LawssssCat/rime-aurora
    - 加入字数输入限制，防止误操作而卡死

---
+++ Lua/code_limit.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+local kRejected = 0 -- 输入法拒绝处理
+local kAccepted = 1 -- 输入法接受处理，并由本processor处理
+local kNoop = 2 -- 交由输入法下一个processor判断是否处理
+
+function M.func(key, env)
+  local ctx = env.engine.context
+  local config = env.engine.schema.config
+
+  -- 限制
+  local length_limit = config:get_string(env.name_space .."/length_limit")
+  if(length_limit~=nil) then
+    if(string.len(ctx.input) > tonumber(length_limit)) then
+      -- ctx:clear()
+      ctx:pop_input(1)
+      return kAccepted
+    end
+  end
+
+  -- 放行
+  return kNoop
+end
+
+return M
