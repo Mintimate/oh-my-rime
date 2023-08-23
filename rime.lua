25eac75bce8f11acd652502b02092c6f9b91a212
Mintimate <mintimate@outlook.com>
2023-08-23 14:45:15 +0800

    bug(no_luu)

--- rime.lua
+++ rime.lua
@@ -1,7 +1,7 @@
-time_translator = require("time")
-date_translator = require("date")
-week_translator = require("week")
-number_translator = require("number_translator")
-code_length_limit_processor = require("code_limit")
+--- time_translator = require("time")
+-- date_translator = require("date")
+-- week_translator = require("week")
+-- number_translator = require("number_translator")
+-- code_length_limit_processor = require("code_limit")
 -- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
-reduce_english_filter = require("reduce_english_filter")
+-- reduce_english_filter = require("reduce_english_filter")
