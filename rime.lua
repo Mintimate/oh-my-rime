4616f5240e65d42c7d3651322729a9a3c1d7f3e5
YummyCocoa <132128788+YummyCocoa@users.noreply.github.com>
2023-12-06 10:53:16 +0800

    Revert "feat(词库更新): 同步雾凇拼音、扩充拆字 (#16)"
    This reverts commit 4c0395b697bf31d67a91f9101a925ae818e742d1.

--- rime.lua
+++ rime.lua
@@ -5,5 +5,3 @@ number_translator = require("number_translator")
 code_length_limit_processor = require("code_limit")
 -- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
 reduce_english_filter = require("reduce_english_filter")
--- 错音错字提示
-corrector = require("corrector")
