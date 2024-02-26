90f67348bfca1b07980a7ed43f7117361e8d4bd1
Mintimate <mintimate215@gmail.com>
2024-02-26 21:20:55 +0800

    fix: 移除__inculde配置，优化字符限制Lua
    经过测试，__include和path的/+配置冲突，虽然可以使用/+/+进行补充。
    
    Fix #28

--- lua/code_length_limit.lua
+++ lua/code_length_limit.lua
@@ -4,12 +4,17 @@ local kRejected = 0 -- 输入法拒绝处理
 local kAccepted = 1 -- 输入法接受处理，并由本processor处理
 local kNoop = 2 -- 交由输入法下一个processor判断是否处理
 
+function M.init(env)
+  local config = env.engine.schema.config
+  env.name_space = env.name_space:gsub('^*', '')
+end
+
 function M.func(key, env)
   local ctx = env.engine.context
   local config = env.engine.schema.config
 
   -- 限制
-  local length_limit = config:get_string(env.name_space .."/length_limit")
+  local length_limit = config:get_string(env.name_space)
   if(length_limit~=nil) then
     if(string.len(ctx.input) > tonumber(length_limit)) then
       -- ctx:clear()
--- rime.lua
+++ rime.lua
@@ -2,7 +2,6 @@ time_translator = require("time")
 date_translator = require("date")
 week_translator = require("week")
 number_translator = require("number_translator")
-code_length_limit_processor = require("code_limit")
 -- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
 reduce_english_filter = require("reduce_english_filter")
 -- 错音错字提示
