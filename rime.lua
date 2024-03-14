18e0ae7aa52773d8dd7e15a4ad15a8c91bc9e6d9
YummyCocoa <mintimate@outlook.com>
2024-03-14 15:58:43 +0800

    !feat: 全面使用新的Lua引入
    全面使用新的Lua引入方式，以备添加到仓更新源内

--- rime.lua
+++ rime.lua
@@ -1,14 +1,1 @@
-time_translator = require("time")
-date_translator = require("date")
-week_translator = require("week")
-number_translator = require("number_translator")
--- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
-reduce_english_filter = require("reduce_english_filter")
--- 错音错字提示
-corrector = require("corrector")
--- 中国农历
-Chinese_lunar_calendar = require("chineseLunarCalendar")
--- 以词定字（默认为「[」和「]」；在 key_binder 下配置快捷键
-select_character = require("select_character")
--- 自动大写英文词汇
-autocap_filter = require("autocap_filter")
+-- 20240314 使用新的引入语法，rime.lua 内部文件去除
